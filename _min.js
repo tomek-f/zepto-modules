88c4983a061fc09bffffe2fae6722e5388ced9b5
Tomasz Fijoł <tomekfijol@gmail.com>
2016-01-01 11:50:26 +0100

    ignore; fix custom config; min config; package.json

--- _min.js
+++ _min.js
@@ -1,11 +1,7 @@
 var $ = require('./zepto');
 
 require('./event');
-require('./form');
 require('./ie');
-require('./detect');
-require('./data');
-require('./touch');
 require('./stack');
 
 module.exports = $;
